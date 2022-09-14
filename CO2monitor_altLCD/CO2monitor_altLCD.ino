c6c3212b7dedee26b874c3de2d6f3fed7c1a587d
Karlduino <karlduino@gmail.com>
2022-09-13 19:03:56 -0500

    Revise arduino sketch for alternate LCD display
    - uses RW1063 controller, which is the same as HD44780
    - need to use LiquidCrystalWired library
    - address is 0x3C

--- CO2monitor_altLCD/CO2monitor_altLCD.ino
+++ CO2monitor_altLCD/CO2monitor_altLCD.ino
@@ -1,9 +1,10 @@
 // CO2 monitor with Senseair S8 and 16x2 LCD display
+// this version is for an alternate LCD display using the RW1063 controller
+// needs LiquidCrystalWired library + address 0x3C
 
 #include <Arduino.h>
 #include "s8_uart.h"
-#include <Wire.h>
-#include <LCD_I2C.h>
+#include "LiquidCrystalWired.h"
 
 #define STARTUP_DELAY_MS    8000
 #define TIME_BETW_UPDATE_MS 5000
@@ -29,12 +30,6 @@
 SoftwareSerial S8_serial(S8_TX_PIN, S8_RX_PIN);
 
 // set up special characters for LCD display
-#if defined(ARDUINO) && ARDUINO >= 100
-#define printByte(args)  write(args);
-#else
-#define printByte(args)  print(args,BYTE);
-#endif
-
 uint8_t subscript2[8] = {0x0, 0x0, 0x0, 0x1E, 0x03, 0x06, 0x0C, 0x1F };
 uint8_t heart[8] = {0x0,0xa,0x1f,0x1f,0xe,0x4,0x0};
 uint8_t smile[8] = {0x0,0x0,0xa,0x0,0x0,0x11,0xe,0x0};
@@ -43,19 +38,20 @@ uint8_t frown[8] = {0x0,0x0,0xa,0x0,0x0,0xe,0x11,0x0};
 S8_UART *sensor_S8;
 S8_sensor sensor;
 
-LCD_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+#define LCD_ADDRESS 0x3c
+LiquidCrystalWired lcd = LiquidCrystalWired(2, 16, FONT_SIZE_5x8, BITMODE_8_BIT);
 
 void setup() {
 
   // start up the LCD display
-  lcd.begin();
-  lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.begin(LCD_ADDRESS, &Wire);
+  lcd.turnOn();
+  lcd.setCursorPosition(0,0);
   lcd.print("Starting up...");
-  lcd.createChar(0, subscript2);
-  lcd.createChar(1, heart);
-  lcd.createChar(2, smile);
-  lcd.createChar(3, frown);
+  lcd.setCustomSymbol(CUSTOM_SYMBOL_1, subscript2);
+  lcd.setCustomSymbol(CUSTOM_SYMBOL_2, heart);
+  lcd.setCustomSymbol(CUSTOM_SYMBOL_3, smile);
+  lcd.setCustomSymbol(CUSTOM_SYMBOL_4, frown);
 
   // Initialize S8 sensor
   S8_serial.begin(S8_BAUDRATE);
@@ -73,11 +69,11 @@ void setup() {
   }
 
   // Show basic S8 sensor info
-  lcd.setCursor(0,0);
+  lcd.setCursorPosition(0,0);
   lcd.print("SenseAir S8      ");
-  lcd.setCursor(0,1);
+  lcd.setCursorPosition(1,0);
   lcd.print("Firmware:        ");
-  lcd.setCursor(10,1);
+  lcd.setCursorPosition(1,10);
   if (len == 0) lcd.print("N/A");
   else lcd.print(sensor.firm_version);
   sensor.sensor_id = sensor_S8->get_sensor_ID();
@@ -85,9 +81,9 @@ void setup() {
   delay(2000);
 
   // wait a bit to try to avoid first reading of 0 ppm
-  lcd.setCursor(0,1);
+  lcd.setCursorPosition(1,0);
   lcd.print("                ");
-  lcd.setCursor(0,0);
+  lcd.setCursorPosition(0,0);
   lcd.print("Warming up...   ");
 
   // beating heart
@@ -95,11 +91,11 @@ void setup() {
     int n_beats = (STARTUP_DELAY_MS - n_tries*250)/1000 + 1;
 
     for(int beat=0; beat < n_beats; beat++) {
-      lcd.setCursor(14,1);
-      lcd.printByte(1);
+        lcd.setCursorPosition(1,14);
+      lcd.printCustomSymbol(CUSTOM_SYMBOL_2);
       delay(800);
 
-      lcd.setCursor(14,1);
+      lcd.setCursorPosition(1,14);
       lcd.print(" ");
       delay(200);
     }
@@ -107,15 +103,15 @@ void setup() {
 
   // constant part of the display
   lcd.clear();
-  lcd.setCursor(5,0);
+  lcd.setCursorPosition(0,5);
   lcd.print("ppm CO");
-  lcd.setCursor(11,0);
-  lcd.printByte(0);  // subscript 2
+  lcd.setCursorPosition(0,11);
+  lcd.printCustomSymbol(CUSTOM_SYMBOL_1);  // subscript 2
 
-  lcd.setCursor(0,1);
+  lcd.setCursorPosition(1,0);
   lcd.print("max CO  =");
-  lcd.setCursor(6,1);
-  lcd.printByte(0);  // subscript 2
+  lcd.setCursorPosition(1,6);
+  lcd.printCustomSymbol(CUSTOM_SYMBOL_1);  // subscript 2
 }
 
 int max_co2 = 0;
@@ -125,38 +121,38 @@ void loop() {
   char buffer[17]; // to hold formated print
 
   // clear bits from the display
-  lcd.setCursor(0,0);
+  lcd.setCursorPosition(0,0);
   lcd.print("    ");
-  lcd.setCursor(10,1);
+  lcd.setCursorPosition(1,10);
   lcd.print("    ");
-  lcd.setCursor(13, 0);
+  lcd.setCursorPosition(0,13);
   lcd.print("   ");
-  lcd.setCursor(15, 1);
+  lcd.setCursorPosition(1,15);
   lcd.print(" ");
 
   // Get and display CO2 measure
   sensor.co2 = sensor_S8->get_co2();
-  lcd.setCursor(0,0);
+  lcd.setCursorPosition(0,0);
   sprintf(buffer, "%4d", sensor.co2);
   lcd.print(buffer);
 
   // max since start-up
   if (sensor.co2 > max_co2) { max_co2 = sensor.co2; }
-  lcd.setCursor(10,1);
+  lcd.setCursorPosition(1,10);
   sprintf(buffer, "%4d", max_co2);
   lcd.print(buffer);
 
   // print warning
-  lcd.setCursor(15,0);
+  lcd.setCursorPosition(0,15);
   if(sensor.co2 <= SAFE_LEVEL) {
-    lcd.printByte(2); // smile
+    lcd.printCustomSymbol(CUSTOM_SYMBOL_3); // smile
   } else if(sensor.co2 < BAD_LEVEL) {
-    lcd.printByte(3); // frown
+    lcd.printCustomSymbol(CUSTOM_SYMBOL_4); // frown
   } else {
-    lcd.setCursor(13,0);
+      lcd.setCursorPosition(0,13);
     lcd.print("!!!");
-    lcd.setCursor(15,1);
-    lcd.printByte(3); // frown
+    lcd.setCursorPosition(1,15);
+    lcd.printCustomSymbol(CUSTOM_SYMBOL_4); // frown
   }
 
   // Wait for selected time before next measure
